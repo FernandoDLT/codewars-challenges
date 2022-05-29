d440b76286274f0f3b5a6dab175b3aff4be0ee91
FERNANDO DE LA TORRE <96254499+ferndiddy@users.noreply.github.com>
2022-05-28 19:28:22 -0500

    init

---
+++ 8kyu-callenges/are-you-playing-banjo/main.js
@@ -0,0 +1,17 @@
+// Are You Playing Banjo?
+// JavaScript:
+function areYouPlayingBanjo(name) {
+  return name + (name[0].toLowerCase() == 'r' ? ' plays' : ' does not play') + " banjo";
+}
+
+function areYouPlayingBanjo(name) {
+  if (name[0].toLowerCase() === 'r') {
+    return name + ' plays banjo';
+  } else {
+    return name + ' does not play banjo';
+  }
+}
+console.log(areYouPlayingBanjo('Fred'))
+console.log(areYouPlayingBanjo('Ralph'))
+console.log(areYouPlayingBanjo('Rita'))
+console.log(areYouPlayingBanjo('Carl'))
