c3972faa9f7e7ea66a87274951a1fa618b30510e
Fernando <ferchodlt1971@gmail.com>
2022-05-30 16:54:11 -0500

    added 2nd option w/'const"

--- 8kyu-callenges/remove-first-and-last-char/main.js
+++ 8kyu-callenges/remove-first-and-last-char/main.js
@@ -2,9 +2,11 @@
 // It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string.
 // You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
 
-function removeFirstAndLastChar(str){
-    return str.slice(1,-1)
-}
+// function removeFirstAndLastChar(str){
+//     return str.slice(1,-1)
+// }
+const removeFirstAndLastChar = str => str.slice(1,-1)
+
 console.log(removeFirstAndLastChar('Carl'))
 console.log(removeFirstAndLastChar('Pedro'))
 console.log(removeFirstAndLastChar('Jaguar'))
