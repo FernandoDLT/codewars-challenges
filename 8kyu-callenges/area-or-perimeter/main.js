1bf7f86a7e3cff3ca5b5e1664124f25ec089be01
Fernando <ferchodlt1971@gmail.com>
2022-06-05 23:13:37 -0500

    init

---
+++ 8kyu-callenges/area-or-perimeter/main.js
@@ -0,0 +1,31 @@
+// Area or Perimeter
+
+// You are given the length and width of a 4-sided polygon. The polygon can either be a 
+// rectangle or a square.
+// If it is a square, return its area. If it is a rectangle, return its perimeter.
+// Example(Input1, Input2 --> Output):
+// 6, 10 --> 32
+// 3, 3 --> 9
+
+const areaOrPerimeter = function(l , w) {
+    if(l==w){
+    return l*w;
+    }
+    else{
+      (l>w)
+      return l*2+w*2;
+    }
+  }
+
+//  const areaOrPerimeter = function(l , w) {
+//     let area = l * w;
+//     let perimeter = (l + w) * 2;
+    
+//     return l === w ? area : perimeter;
+//   };
+  
+        // *****SHORTHAND OPTION *****
+// const areaOrPerimeter = (l, w) => l === w ? l * w : (l + w) * 2;
+
+  console.log(areaOrPerimeter(6,10)) // Retrns 32
+  console.log(areaOrPerimeter(3,3)) // Returns 9
