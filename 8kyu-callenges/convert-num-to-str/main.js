f7b934ffd25cda68f83f8a7cdd3f49b5f27987b9
Fernando <ferchodlt1971@gmail.com>
2022-06-06 14:04:32 -0500

    init

---
+++ 8kyu-callenges/convert-num-to-str/main.js
@@ -0,0 +1,10 @@
+// CONVERT A NUMBER TO A STRING
+
+// We need a function that can transform a number into a string.
+// What ways of achieving this do you know?
+
+function numToStr(num){
+    return num.toString()
+}
+console.log(numToStr(123))
+console.log(numToStr(999))
