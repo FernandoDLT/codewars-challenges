1e9c20a6e2d64985031032d0864ee0d2fed75265
Fernando <ferchodlt1971@gmail.com>
2022-06-14 23:46:45 -0500

    lots-of-practice

--- 7kyu-challenges/hi-and-lo-numbers/main.js
+++ 7kyu-challenges/hi-and-lo-numbers/main.js
@@ -10,7 +10,7 @@
 // All numbers are valid Int32, no need to validate them.
 // There will always be at least one number in the input string.
 // Output string must be two numbers separated by a single space, and highest number is first.
-
+// Did lots of practice on this one...
 
 function highAndLow(numbers){
     const arr = numbers.split(" ").map(Number)
